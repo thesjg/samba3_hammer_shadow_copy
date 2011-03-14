a4cb7648aa0d2ea0aa04d883b1a61c12a3eeca90
Samuel J. Greear <sjg@thesjg.com>
2011-03-13 22:52:08 -0600

    Add some rudimentary @GMT->@@TID munging

--- hammer_shadow_copy.c
+++ hammer_shadow_copy.c
@@ -7,11 +7,11 @@
  * a list of available "Volume labels" (snapshots), windows begins stat'ing
  * them in sequence using some combination of the path, the filename and the
  * @GMT formatted Volume label. For example,
- *
- * path/@GMT-*/to/file
- * @GMT-*/path/to.file
- * ./@GMT-*/file
- *
+ */
+ // path/@GMT-*/to/file
+ // @GMT-*/path/to.file
+ // ./@GMT-*/file
+/*
  * These are translated into absolute pathnames corresponding to snapshots
  * using the HAMMER TID format by this module. Windows looks at the stat
  * results of each valid return and lists all versions of the file which it
@@ -24,6 +24,7 @@
 // XXX
 
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
 
@@ -56,14 +57,18 @@ hammer_free_snapshots(hammer_snapshots_t snapshots)
 
 static
 hammer_snapshots_t
-hammer_get_snapshots(TALLOC_CTX *mem_ctx, int fd)
+hammer_get_snapshots(TALLOC_CTX *mem_ctx, char *path)
 {
     struct hammer_ioc_pseudofs_rw ioc_pfs;
     struct hammer_pseudofs_data pfs_data;
     struct hammer_ioc_info ioc_info;
     struct hammer_ioc_snapshot ioc_snapshot;
     hammer_snapshots_t ret_snapshots;
-    int i;
+    int i, fd;
+
+    fd = open(path, O_RDONLY);
+    if (fd < 0)
+        goto fail;
 
     memset(&ioc_pfs, 0, sizeof(ioc_pfs));
     memset(&pfs_data, 0, sizeof(pfs_data));
@@ -71,16 +76,16 @@ hammer_get_snapshots(TALLOC_CTX *mem_ctx, int fd)
     ioc_pfs.ondisk = &pfs_data;
     ioc_pfs.bytes = sizeof(pfs_data);
     if (ioctl(fd, HAMMERIOC_GET_PSEUDOFS, &ioc_pfs) < 0)
-        return (NULL);
+        goto fail;
 
     memset(&ioc_info, 0, sizeof(ioc_info));
     if (ioctl(fd, HAMMERIOC_GET_INFO, &ioc_info) < 0)
-        return (NULL);
+        goto fail;
 
     ret_snapshots = (hammer_snapshots_t)
         talloc_size(mem_ctx, sizeof(*ret_snapshots));
     if (ret_snapshots == NULL)
-        return (NULL);
+        goto fail;
 
     ret_snapshots->count = 0;
     TAILQ_INIT(&ret_snapshots->snaps);
@@ -111,8 +116,82 @@ hammer_get_snapshots(TALLOC_CTX *mem_ctx, int fd)
     return (ret_snapshots);
 
 fail:
+    close(fd);
     hammer_free_snapshots(ret_snapshots);
     return (NULL);
+}
+
+#define HAMMER_GMT_LABEL_PREFIX		"@GMT-"
+#define HAMMER_GMT_LABEL_FORMAT		"@GMT-%Y.%m.%d-%H.%M.%S"
+
+static
+char *
+hammer_match_gmt(const char *name)
+{
+    unsigned year, month, day, hour, min, sec;
+    char *ret = NULL;
+
+syslog(LOG_CRIT, "HAMMER: hammer_match_gmt: %s", name);
+
+    ret = strnstr(name, HAMMER_GMT_LABEL_PREFIX, strlen(name));
+    if (ret == NULL)
+        return (NULL);
+
+    if (sscanf(ret, "@GMT-%04u.%02u.%02u-%02u.%02u.%02u",
+        &year, &month, &day, &hour, &min, &sec) != 6)
+    {
+        return (NULL);
+    }
+
+    return (ret);
+}
+
+static
+char *
+hammer_replace_gmt_tid(const char *path, hammer_tid_t tid)
+{
+    char *offset, *ret = NULL;
+
+syslog(LOG_CRIT, "HAMMER: hammer_replace_gmt_tid: ent: %s", path);
+
+    ret = talloc_strdup(talloc_tos(), path);
+    offset = hammer_match_gmt(ret);
+    if (offset != NULL) {
+        int i = strlen(offset);
+        snprintf(offset, 20+1, "@@0x%016jx", (uintmax_t)tid);
+        strncpy(offset + 20, offset + 24, i - 24);
+        offset[i-4] = '\0';
+    }
+
+syslog(LOG_CRIT, "HAMMER: hammer_replace_gmt_tid: exit: %s", ret);
+
+    return (ret);
+}
+
+static
+char *
+hammer_strip_gmt(const char *path)
+{
+    char *offset, *ret = NULL;
+
+syslog(LOG_CRIT, "HAMMER: hammer_strip_gmt: ent: %s", path);
+
+    ret = talloc_strdup(talloc_tos(), path);
+    offset = hammer_match_gmt(ret);
+    if (offset != NULL) {
+        if (strlen(offset) == 24) {
+syslog(LOG_CRIT, "HAMMER: hammer_strip_gmt: offset is 24 (pure GMT)");
+            ret = ".";
+            return (ret);
+        }
+
+        strncpy(offset, offset + 25, strlen(offset) - 25);
+        offset[strlen(offset)-25] = '\0'; 
+   }
+
+syslog(LOG_CRIT, "HAMMER: hammer_strip_gmt: ex: %s", ret);
+
+    return (ret);
 }
 
 static
@@ -136,17 +215,42 @@ hammer_stat(vfs_handle_struct *handle, struct smb_filename *smb_fname)
 {
     int ret = SMB_VFS_NEXT_STAT(handle, smb_fname);
     char *l = NULL;
+    hammer_snapshots_t snapshots;
+    hammer_snapshot_t snapshot;
+    char *s, *g, *cwd;
 
-/* XXX: MAD HAX BEWARE ZOMG */
+cwd = getcwd(NULL, MAXPATHLEN);
+syslog(LOG_CRIT, "HAMMER: hammer_stat, cwd is: %s", cwd);
 
-#define SHADOW_COPY_PREFIX "@GMT-"
-    l = strnstr(smb_fname->base_name, SHADOW_COPY_PREFIX, sizeof(SHADOW_COPY_PREFIX));
-    if (l != NULL) {
-        strncpy(l, "@@0x00000002058622d0", 20);
-        l += 20;
-        strncpy(l, l + 4, strlen(l + 4));
-        smb_fname->base_name[strlen(smb_fname->base_name) - 4] = '\0';
-        ret = SMB_VFS_NEXT_STAT(handle, smb_fname);
+    g = hammer_match_gmt(smb_fname->base_name);
+    if (g != NULL) {
+        int i = 0;
+
+        s = hammer_strip_gmt(smb_fname->base_name);
+syslog(LOG_CRIT, "HAMMER: hammer_stat: getting snapshots: %s", s);
+        snapshots = hammer_get_snapshots(talloc_tos(), s);
+        if (snapshots == NULL) {
+syslog(LOG_CRIT, "HAMMER: hammer_stat: FAILED getting snapshots: %s", s);
+            return (ret);
+        }
+syslog(LOG_CRIT, "HAMMER: hammer_stat: got %d snapshots", snapshots->count);
+
+        TAILQ_FOREACH(snapshot, &snapshots->snaps, snap) {
+            time_t t = snapshot->ts / 1000000ULL;
+            struct tm *tp = gmtime(&t);
+            char tlabel[100];
+
+syslog(LOG_CRIT, "LOOP ITER XXX: %d", i++);
+            strftime(&tlabel[0], sizeof(tlabel), HAMMER_GMT_LABEL_FORMAT, tp);
+syslog(LOG_CRIT, "HAMMER: trying label %s against %s", tlabel, g);
+            if (strncmp(g, tlabel, 24) == 0) {
+                smb_fname->base_name = hammer_replace_gmt_tid(smb_fname->base_name,
+                                                              snapshot->tid);
+syslog(LOG_CRIT, "HAMMER: changed base_name to: %s", smb_fname->base_name);
+                ret = SMB_VFS_NEXT_STAT(handle, smb_fname);
+                break;
+            }
+        }
     }
 
     return (ret);
@@ -183,34 +287,25 @@ syslog(LOG_CRIT, "HAMMER: hammer_opendir: path: %s, mask: %s", path, mask);
     return (sd);
 }
 
-
-
-#define GMT_LABEL_FORMAT	"@GMT-%Y.%m.%d-%H.%M.%S"
-
 static
 int
 hammer_get_shadow_copy_data(vfs_handle_struct *handle, files_struct *fsp,
                             SHADOW_COPY_DATA *shadow_copy_data, bool labels)
 {
     int filled_labels = 0;
-    int fd, error = -1;
+    int error = -1;
     hammer_snapshots_t snapshots;
     hammer_snapshot_t snapshot;
 
     shadow_copy_data->num_volumes = 0;
     shadow_copy_data->labels = NULL;
 
-    fd = open(fsp->conn->connectpath, O_RDONLY);
-    if (fd < 0)
-        return (error);
-
-    snapshots = hammer_get_snapshots(shadow_copy_data->mem_ctx, fd);
+    snapshots = hammer_get_snapshots(shadow_copy_data->mem_ctx,
+        fsp->conn->connectpath);
     if (snapshots == NULL) {
-        close(fd);
         return (error);
     }
 
-snapshots->count = 3; // XXX
 syslog(LOG_CRIT, "HAMMER: Got %d snapshots for file/dir", snapshots->count);
     shadow_copy_data->num_volumes = snapshots->count;
     error = 0;
@@ -223,13 +318,13 @@ syslog(LOG_CRIT, "Filling labels");
             goto done;
 
         TAILQ_FOREACH(snapshot, &snapshots->snaps, snap) {
+
             time_t t = snapshot->ts / 1000000ULL;
             struct tm *tp = gmtime(&t);
             strftime(rlabels[filled_labels++], sizeof(*rlabels),
-                GMT_LABEL_FORMAT, tp);
+                HAMMER_GMT_LABEL_FORMAT, tp);
 
             syslog(LOG_CRIT, "HAMMER: Adding label: %s", rlabels[filled_labels-1]);
-if (filled_labels == 3) break; // XXX
         }
 
         shadow_copy_data->labels = rlabels;
@@ -239,13 +334,12 @@ syslog(LOG_CRIT, "HAMMER: Filled %d labels", filled_labels);
 syslog(LOG_CRIT, "HAMMER: Found %d snapshots", shadow_copy_data->num_volumes);
 
 done:
-    close(fd);
     hammer_free_snapshots(snapshots);
     return (error);
 }
 
 
-static struct vfs_fn_pointers vfs_hammer_shadow_copy_fns = {
+static struct vfs_fn_pointers hammer_shadow_copy_fns = {
     .fstat = hammer_fstat,
     .lstat = hammer_lstat,
     .stat = hammer_stat,
@@ -257,7 +351,7 @@ static struct vfs_fn_pointers vfs_hammer_shadow_copy_fns = {
 NTSTATUS init_samba_module(void)
 {
     NTSTATUS ret = smb_register_vfs(SMB_VFS_INTERFACE_VERSION,
-        "vfs_hammer_shadow_copy", &vfs_hammer_shadow_copy_fns);
+        "hammer_shadow_copy", &hammer_shadow_copy_fns);
 
     if (!NT_STATUS_IS_OK(ret))
         return (ret);
